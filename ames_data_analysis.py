e3cf00c3a6ca279022e68ea6186577f0ee617722
DHYANI\DHYANI <vdhyani96@gmail.com>
2019-10-19 20:25:05 -0400

    Code for Permutation test, custom built distance metric and clustering using the metric

--- ames_data_analysis.py
+++ ames_data_analysis.py
@@ -10,6 +10,11 @@ import pandas as pd
 import seaborn as sns
 import os
 from matplotlib import pyplot as plt
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.cluster import KMeans
+import hdbscan
 
 os.chdir('C:\\Users\\admin\\Desktop\\PostG\\GRE\\Second Take\\Applications\\Univs\\Stony Brook\\Fall 19 Courses\\DSF\\Homework 3\\house-prices-advanced-regression-techniques')
 
@@ -175,6 +180,7 @@ d_norml['d_index'] = ((d_norml['d_index'] - d_norml['d_index'].min())/
 
 d_norml['SalePrice'] = train['SalePrice']
 # check correlation between the d_index and SalePrice
+sns.lmplot(data = d_norml[['d_index', 'SalePrice']], x='d_index', y='SalePrice')
 d_norml[['d_index', 'SalePrice']].corr()
 # 0.80345 - This is even higher than the correlation of all of the individual features!
 
@@ -192,6 +198,15 @@ print(dhouses.head(10))
 # 10 least desirable houses
 print(dhouses.tail(10))
 
+# now create a master table for average desirability score across all neighborhoods
+master = pd.DataFrame({'Neighborhood':train['Neighborhood'], 'd_index':d_norml['d_index']})
+master = master.groupby('Neighborhood')['d_index'].mean()
+
+# desirability of neighborhoods based on average d_index
+sns.barplot(master, master.index)
+# in tabular form
+print(master.sort_values(ascending=False))
+
 
 ## 4. Pairwise Distance Function - measuring the similarity of two properties
 # Similar properties can be thought of having similar prices. Since, the desirability
@@ -201,9 +216,12 @@ print(dhouses.tail(10))
 # large values for dissimilar houses. To magnify that range, I can use an exponential
 # function as well
 # To test, I can pick 3 houses and find the similarity between two pairs
-house0 = d_norml['d_index'][0]
-house1 = d_norml['d_index'][1]
-house3 = d_norml['d_index'][3]
+# can also include normalized SalePrice
+d_norml['SalePrice'] = ((d_norml['SalePrice'] - d_norml['SalePrice'].min())/
+                        (d_norml['SalePrice'].max() - d_norml['SalePrice'].min()))
+house0 = d_norml['d_index'][0] + d_norml['SalePrice'][0]
+house1 = d_norml['d_index'][1] + d_norml['SalePrice'][1]
+house3 = d_norml['d_index'][3] + d_norml['SalePrice'][3]
 distance01 = 100**(abs(house0 - house1))
 distance13 = 100**(abs(house1 - house3))
 print(distance01)
@@ -211,9 +229,9 @@ print(distance13)
 # From above, we can see that the distance between house1 and house3 is greater than
 # the distance between house0 and house1. We can verify it using the SalePrice difference
 # as well
-Price0 = d_norml['SalePrice'][0]
-Price1 = d_norml['SalePrice'][1]
-Price3 = d_norml['SalePrice'][3]
+Price0 = train['SalePrice'][0]
+Price1 = train['SalePrice'][1]
+Price3 = train['SalePrice'][3]
 Price01 = abs(Price0 - Price1)
 Price13 = abs(Price1 - Price3)
 print(Price01)
@@ -221,17 +239,17 @@ print(Price13)
 # We see that Price difference between house1 and house3 is also greater than the price
 # difference between house0 and house1.
 # Another test with different set of houses:
-house196 = d_norml['d_index'][196]
-house990 = d_norml['d_index'][990]
-house165 = d_norml['d_index'][165]
+house196 = d_norml['d_index'][196] + d_norml['SalePrice'][196]
+house990 = d_norml['d_index'][990] + d_norml['SalePrice'][990]
+house165 = d_norml['d_index'][165] + d_norml['SalePrice'][165]
 distance196_990 = 100**(abs(house196 - house990))
 distance196_165 = 100**(abs(house196 - house165))
 print(distance196_990)
 print(distance196_165)
 # now price comparison
-Price196 = d_norml['SalePrice'][196]
-Price990 = d_norml['SalePrice'][990]
-Price165 = d_norml['SalePrice'][165]
+Price196 = train['SalePrice'][196]
+Price990 = train['SalePrice'][990]
+Price165 = train['SalePrice'][165]
 Price196_990 = abs(Price196 - Price990)
 Price196_165 = abs(Price196 - Price165)
 print(Price196_990)
@@ -247,6 +265,263 @@ print(Price196_165)
 # manifests in some other form - like structure of the house, number of rooms, etc - then 
 # this distance metric might not work. 
 
+
+## 5. Clustering of houses using the distance metric
+
+sns.lmplot(data = d_norml[['d_index', 'SalePrice']], x='d_index', y='SalePrice')
+# Can we cluster the above points using the distance metric?
+# Would need to create a (square) pairwise distance matrix first using my distance logic
+# separate the d_index and SalePrice
+clus = np.array(d_norml[['d_index', 'SalePrice']])
+# Compute the pairwise distance matrix using my distance metric
+distance_matrix = pairwise_distances(clus, 
+                        metric=lambda x, y: 100**(abs((x[0]+x[1])-(y[0]+y[1]))))
+
+# Now run the HDBSCAN clustering algorithm on the data points
+clusterer = hdbscan.HDBSCAN(metric='precomputed')
+clusterer.fit(distance_matrix)
+clusterer.labels_
+clusterer.labels_.max()
+# Only three clusters were produced
+print(np.unique(clusterer.labels_, return_counts=True))
+
+# visualizing
+color_palette = sns.color_palette('deep', 8)
+cluster_colors = [color_palette[x] if x >= 0
+                  else (0.5, 0.5, 0.5)
+                  for x in clusterer.labels_]
+cluster_member_colors = [sns.desaturate(x, p) for x, p in
+                         zip(cluster_colors, clusterer.probabilities_)]
+plt.scatter(*clus.T, s=50, linewidth=0, c=cluster_member_colors, alpha=0.25)
+
+train['Neighborhood'][clusterer.labels_==0]
+# This cluster has houses in Neighborhoods that are new and sell for higher prices
+# mostly NridgeHt, NoRidge and StoneBr
+train['Neighborhood'][clusterer.labels_==2]
+# This cluster also has some specific Neighborhoods where houses are new and sell for
+# high prices, like NoRidge, StoneBr, but it also has some other mix of Neighborhoods
+train['Neighborhood'][clusterer.labels_==1]
+# All the other Neighborhoods (apart from the noise) are in this cluster. This is the
+# biggest cluster of all
+train['Neighborhood'][clusterer.labels_==-1]
+# This is the list of noise or outliers
+
+# Can try any other clustering if I want - Kmeans
+kmeans = KMeans(n_clusters=5, random_state=519).fit(distance_matrix)
+print(np.unique(kmeans.labels_, return_counts=True))
+# visualizing
+new = np.append(clus, np.reshape(kmeans.labels_, (1460,1)), axis=1)
+new = pd.DataFrame({'Col1':new[:, 0], 'Col2':new[:, 1], 'Cluster':new[:, 2]+1})
+color_palette = sns.color_palette('deep', 5)
+sns.scatterplot(data=new, x='Col1', y='Col2', hue='Cluster', legend='full', palette=color_palette)
+
+# Let's check the distribution of neighborhoods in each clusters
+# 1st cluster
+var = np.unique(train['Neighborhood'][kmeans.labels_==0], return_counts=True)
+var1 = np.array(var[0], dtype='str')
+var2 = var[1]
+var = pd.DataFrame({'Neighborhood': var1, 'Count': var2})
+
+# join var with the master table
+var = var.merge(master, left_on='Neighborhood', right_on=master.index, how='inner')
+var['desirability'] = 'low'
+var['desirability'][var['d_index']>0.2] = 'moderate'
+var['desirability'][var['d_index']>0.3] = 'high'
+var['desirability'][var['d_index']>0.4] = 'very high'
+sns.barplot(data=var, y='Neighborhood', x='Count', hue='desirability')
+# As we can see, this cluster consists of a mix of moderate, high and very highly desirable neighborhoods
+
+# 2nd Cluster
+var = np.unique(train['Neighborhood'][kmeans.labels_==1], return_counts=True)
+var1 = np.array(var[0], dtype='str')
+var2 = var[1]
+var = pd.DataFrame({'Neighborhood': var1, 'Count': var2})
+
+# join var with the master table
+var = var.merge(master, left_on='Neighborhood', right_on=master.index, how='inner')
+var['desirability'] = 'low'
+var['desirability'][var['d_index']>0.2] = 'moderate'
+var['desirability'][var['d_index']>0.3] = 'high'
+var['desirability'][var['d_index']>0.4] = 'very high'
+
+# This cluster has just a single Neighborhood NoRidge with very high desirability
+
+# 3rd Cluster
+var = np.unique(train['Neighborhood'][kmeans.labels_==2], return_counts=True)
+var1 = np.array(var[0], dtype='str')
+var2 = var[1]
+var = pd.DataFrame({'Neighborhood': var1, 'Count': var2})
+
+# join var with the master table
+var = var.merge(master, left_on='Neighborhood', right_on=master.index, how='inner')
+var['desirability'] = 'low'
+var['desirability'][var['d_index']>0.2] = 'moderate'
+var['desirability'][var['d_index']>0.3] = 'high'
+var['desirability'][var['d_index']>0.4] = 'very high'
+sns.barplot(data=var, y='Neighborhood', x='Count', hue='desirability')
+# From the plot, it looks like this cluster contains mostly low and moderately desirable neighborhoods
+# but most are moderate
+
+# 4th Cluster
+var = np.unique(train['Neighborhood'][kmeans.labels_==3], return_counts=True)
+var1 = np.array(var[0], dtype='str')
+var2 = var[1]
+var = pd.DataFrame({'Neighborhood': var1, 'Count': var2})
+
+# join var with the master table
+var = var.merge(master, left_on='Neighborhood', right_on=master.index, how='inner')
+var['desirability'] = 'low'
+var['desirability'][var['d_index']>0.2] = 'moderate'
+var['desirability'][var['d_index']>0.3] = 'high'
+var['desirability'][var['d_index']>0.4] = 'very high'
+sns.barplot(data=var, y='Neighborhood', x='Count', hue='desirability')
+#plt.xticks(np.arange(min(var['Count']), max(var['Count'])+1, 1.0))
+# This cluster also has low and moderately desirable neighborhoods, but most are low
+
+# 5th cluster
+var = np.unique(train['Neighborhood'][kmeans.labels_==4], return_counts=True)
+var1 = np.array(var[0], dtype='str')
+var2 = var[1]
+var = pd.DataFrame({'Neighborhood': var1, 'Count': var2})
+
+# join var with the master table
+var = var.merge(master, left_on='Neighborhood', right_on=master.index, how='inner')
+var['desirability'] = 'low'
+var['desirability'][var['d_index']>0.2] = 'moderate'
+var['desirability'][var['d_index']>0.3] = 'high'
+var['desirability'][var['d_index']>0.4] = 'very high'
+sns.barplot(data=var, y='Neighborhood', x='Count', hue='desirability')
+plt.xticks(np.arange(min(var['Count']), max(var['Count'])+1, 1.0))
+# This cluster has the most desirable neighborhoods of Ames - NoRidge, NridgHt, and StoneBr
+
+# Tried with 9 clusters but it creates a mix of neighborhoods in each cluster that are not much compatible with the
+# categories of desirability
+
+
+## 6. Linear regression model to predict the SalePrice
+# I will pick the features that are most correlated with the SalePrice
+xlin = d_norml.iloc[:, :-1]
+ylin = d_norml['SalePrice']
+lreg = LinearRegression().fit(xlin, ylin)
+lreg.score(xlin, ylin)
+# The Rsquared value is 0.7132, which shows that about 71.3% variance in the SalePrice is explained
+# by the predictors - not a bad value
+pred = lreg.predict(xlin)
+rmse = np.sqrt(mean_squared_error(ylin, pred))
+print(rmse)
+# rmse = 0.0591
+# Since all the variables were normalized before training, I can simply compare the coefficients of
+# the model to see the most important variable
+lreg.coef_
+# We can see that the most important variable here is GrLivArea (living area above ground) with
+# coeff value = 0.4856
+
+
+
+## 7. Integrate one external dataset to improve prediction
+# 30-Year Fixed Rate Mortgage Average in the United States dataset
+# Freddie Mac, 30-Year Fixed Rate Mortgage Average in the United States [MORTGAGE30US], retrieved from FRED, Federal Reserve Bank of St. Louis; 
+# https://fred.stlouisfed.org/series/MORTGAGE30US, October 19, 2019.
+# Copyright, 2016, Freddie Mac. Reprinted with permission.
+# I will take this dataset and join it with the train dataset on the year of sale, in order to get
+# the mortgage rate for that year
+mgage_data = pd.read_csv('External_dataset_MORTGAGE30US.csv')
+# have to separate the date column into year and month
+mgage_data['Year'] = pd.to_datetime(mgage_data['DATE']).dt.year
+mgage_data['Month'] = pd.to_datetime(mgage_data['DATE']).dt.month
+# now join train with the mgage_data on Year and Month
+enhanced = train.merge(mgage_data, left_on=['YrSold', 'MoSold'], right_on=['Year', 'Month'], how='inner')
+enhanced = enhanced.drop(columns=['DATE', 'Year', 'Month'])
+enhanced.rename(columns={'MORTGAGE30US':'Mortgage'}, inplace=True)
+# check the correlation of mortgage with the SalePrice
+sns.lmplot(data=enhanced, x='Mortgage', y='SalePrice')
+# pretty bad, can try training the model (with normalizing Mortgage)
+xlin2 = d_norml.iloc[:, :-1]
+xlin2['Mortgage'] = enhanced['Mortgage']
+# normalize Mortgage
+xlin2['Mortgage'] = ((xlin2['Mortgage'] - xlin2['Mortgage'].min())/
+                    (xlin2['Mortgage'].max() - xlin2['Mortgage'].min()))
+ylin2 = d_norml['SalePrice']
+lreg2 = LinearRegression().fit(xlin2, ylin2)
+lreg2.score(xlin2, ylin2)
+# The Rsquared value is 0.7134, which is nearly the same as with the model without Mortgage variable
+pred2 = lreg2.predict(xlin2)
+rmse2 = np.sqrt(mean_squared_error(ylin2, pred2))
+print(rmse2)
+# rmse = 0.059, almost the same as previous model
+# Since all the variables were normalized before training, I can simply compare the coefficients of
+# the model to see the most important variable
+lreg2.coef_
+# It turns out that this additional variable Mortgage doesn't help much with the modeling, even though
+# it's a common belief that mortgage rates affect the house prices a lot. 
+# One reason could be that there's not enough data across different time duration capturing the sales
+# of houses. There are just a few unique combination of Year-Month of house sale and for each time
+# duration, the SalePrices vary a lot for different houses. Mortgage alone can't have a strong impact
+# on the housing prices.
+
+
+
+## 8. Permutation tests
+# function to calculate the score rmse log
+def rmselog(pred, y):
+    pred = np.log(pred)
+    y = np.log(y)
+    score = np.sqrt(mean_squared_error(y, pred))
+    return score
+
+# function to perform permutation test with 1000 permutations
+def permutation_test(x, y):
+    scores = []
+    for i in range(1000):
+        y = np.random.permutation(y)
+        lreg = LinearRegression().fit(x, y)
+        pred = lreg.predict(x)
+        score = rmselog(pred, y)
+        scores.append(score)
+    return scores
+
+
+
+# a. GrLivArea
+xh = np.array(train['GrLivArea'])
+xh = np.reshape(xh, (1460,1))
+yh = train['SalePrice']
+lregh = LinearRegression().fit(xh, yh)
+lregh.score(xh, yh)
+predh = lregh.predict(xh)
+scoreh = rmselog(predh, yh) # empirical value
+# pvalue determined by the fraction of scores in the permutation tests that are as extreme (low) as 
+# the empirical value
+# p = no of scores <= emp value / total no of scores
+# get the list of scores
+scores = permutation_test(xh, yh)
+# now calculating the pvalue
+pvalue = np.sum(scores <= scoreh)/len(scores)
+
+# now performing the permutation tests for the other 9 variables 
+variables = ['BsmtFinSF1', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 
+             'TotRmsAbvGrd', 'GarageArea', 'YrSold', 'LotArea', 'OverallCond']
+pvalues = dict()
+yh = train['SalePrice']
+for var in variables:
+    xh = np.array(train[var])
+    xh = np.reshape(xh, (1460,1))
+    lregh = LinearRegression().fit(xh, yh)
+    predh = lregh.predict(xh)
+    scoreh = rmselog(predh, yh) # empirical value
+    scores = permutation_test(xh, yh)
+    # now calculating the pvalue
+    pvalue = np.sum(scores <= scoreh)/len(scores)
+    pvalues[var] = pvalue
+
+pvalues = pd.DataFrame(list(pvalues.items()), columns=['Variables', 'pvalues'])
+print(pvalues)
+# Out of all these variables, only two variables had pvalues > 0.05
+# OverallCond and YrSold, so the models built using these two variables are not very significant
+
+
+
+## 9. Build a strong model for Kaggle submission
 
 
 
